3db2327b100844341ad38687e7cb490cba3cc97a
giacomomazzenga <157354090+giacomomazzenga@users.noreply.github.com>
2024-01-22 18:34:01 +0000

    c

---
+++ file7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+printf ("inserisci un numero\n");
+    int a;
+    scanf ("%d", &a);
+    printf ("inserisci il secondo numero\n");
+    int b;
+    scanf ("%d", &b);
+    int c = a*b;
+printf ("Risultato= %d\n", c);
+
+
+
+
+
+
+
+
+
+
+}
