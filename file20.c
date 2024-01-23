1085a3e483f2721b18c778b575a6f70ea39e795f
giacomomazzenga <157354090+giacomomazzenga@users.noreply.github.com>
2024-01-23 21:37:35 +0000

    .

---
+++ file20.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+
+{
+    char x [50];
+
+    printf("vuoi sapere come funziona la figa?\n");
+    scanf("%s", x);
+    while (strcmp( x, "si") == 0)
+    {
+        printf("allora ascolta, la figa è un'invenzione di einstein e di Praust ed ha tre buchi, uno per la ciola, uno per pisciare, e uno è il buco del culo che se non ben lavato puzza di merda\n");
+        printf("vuoi sapere come funziona la figa?\n");
+        scanf("%s", x);
+    }
+
+}
